f12828c80eb4d1c3061b57bb57a6bf0ceaf064c7
salarelv <taavi.juursalu@3dtech.ee>
2015-09-28 12:36:25 +0300

    Added data loaded broadcasting

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var wfangular = angular.module('wfangular', []);
-wfangular.factory('wfangular3d', function() {
+wfangular.factory('wfangular3d', ['$rootScope', function($rootScope) {
   var wf = new Wayfinder3D();
+  wf.onDataLoaded = function(){
+  	$rootScope.$broadcast('wf.data.loaded', []);
+  }
   return wf;
-});
+}]);
