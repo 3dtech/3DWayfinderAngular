6a9becd758795e00b9246faaa338145a4b2cbe46
Tõnis Terasmaa <tonis.terasmaa@3dwayfinder.com>
2016-02-10 11:25:52 +0200

    Version 0.0.8

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
 # 3DWayfinderAngular
-Angular Service for 3D Wayfinder
+Angular Service for 3D Wayfinder (now supports 2D maps also)
 
-## Getting started 
+## Getting started
 Please include the following JavaScript files into Your HTML file and inject wfangular into Your Angular app:
 
 ```javascript
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.0/jquery.min.js"></script>
 <script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/frak-stable.min.js"></script>
+<!-- FOR 3D Map add the line below -->
 <script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/Wayfinder3D.min.js"></script>
+<!-- FOR 2D Map add the line below -->
+<script type="text/javascript" src="http://static.3dwayfinder.com/projects/shared/js/minified/Wayfinder2D.min.js"></script>
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.6/angular.min.js"></script>
 <script type="text/javascript" src="index.js"></script>
 
+
 <script>
     var app = angular.module('basictest', ['wfangular']);
+    // ------ For 2D engine use the line below instead -------
+    // app.run(['wfangualr2d', function(wayfinder) {
     app.run(['wfangular3d', function(wayfinder) {
       WayfinderAPI.LOCATION = "http://api.3dwayfinder.com/";
       wayfinder.options.assetsLocation = 'http://static.3dwayfinder.com/shared/';
--- index.js
+++ index.js
@@ -12,7 +12,7 @@ wfangular.factory('wfangular3d', ['$rootScope', function($rootScope) {
   wf.cbOnLanguageChange = function(language){
     $rootScope.$broadcast('wf.language.change', language);
   }
- 
+
   wf.cbOnFloorChange = function(floor){
     $rootScope.$broadcast('wf.floor.change', floor);
   }
@@ -28,7 +28,52 @@ wfangular.factory('wfangular3d', ['$rootScope', function($rootScope) {
   return wf;
 }]);
 
+wfangular.factory('wfangular2d', ['$rootScope', function($rootScope) {
+  var wf = new Wayfinder2D();
+  wf.onDataLoaded = function(){
+  	$rootScope.$broadcast('wf.data.loaded', []);
+  }
+
+  wf.cbOnPOIClick = function(poi){
+    $rootScope.$broadcast('wf.poi.click', poi);
+  }
+
+  wf.cbOnLanguageChange = function(language){
+    $rootScope.$broadcast('wf.language.change', language);
+  }
+
+  wf.cbOnFloorChange = function(floor){
+    $rootScope.$broadcast('wf.floor.change', floor);
+  }
+
+  wf.cbOnZoomChange = function(zoom){
+    $rootScope.$broadcast('wf.zoom.change', zoom);
+  }
+
+  wf.onBeforeFloorChange = function(currentFloor, nextFloor, destinationFloor){
+    $rootScope.$broadcast('wf.path.floor.change', {current: currentFloor, next: nextFloor, destination: destinationFloor});
+  }
+
+  return wf;
+}])
+
 wfangular.filter('wfCurrentLanguage', ['wfangular3d', function(wayfinder) {
+  return function(input) {
+  	if(input && typeof input === "object"){
+  		if(input[wayfinder.getLanguage()])
+    		return input[wayfinder.getLanguage()];
+    	else if(input["translations"][wayfinder.getLanguage()])
+    		return input["translations"][wayfinder.getLanguage()];
+    	else
+    		return input;
+  	}
+  	else {
+  		return input;
+  	}
+  };
+}]);
+
+wfangular.filter('wfCurrentLanguage2d', ['wfangular2d', function(wayfinder) {
   return function(input) {
   	if(input && typeof input === "object"){
   		if(input[wayfinder.getLanguage()])
